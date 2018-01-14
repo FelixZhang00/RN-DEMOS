1b62be54fbceb2a8f4d35eda88783b481e97a903
Jamffy <l836828946@gmail.com>
2018-01-14 10:38:04 +0800

    movies

---
+++ api.js
@@ -0,0 +1,5 @@
+import Config from 'react-native-config';
+
+//config from .env file in root project.
+//https://stackoverflow.com/questions/33117227/setting-environment-variable-in-react-native
+//export const OMDB_API_KEY = Config.OMDB_API_KEY;
--- app/movies/components/main.js
+++ app/movies/components/main.js
@@ -4,23 +4,54 @@ import {
     StyleSheet,
     Text,
     View,
-    ListView, TextInput
+    ListView, TextInput, Image
 } from 'react-native'
+import {debounce} from 'lodash'
+
+//import { OMDB_API_KEY} from '../../../api';
+//https://stackoverflow.com/questions/33117227/setting-environment-variable-in-react-native
+import { OMDB_API_KEY} from 'react-native-dotenv'
 
 export default class Main extends Component<{}> {
 
     constructor(props) {
         super(props);
         var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        this.state ={
-            dataSource: ds.cloneWithRows(['first item','second item']),
+        this.state = {
+            dataSource: ds.cloneWithRows([]),
         }
+        this.searchMovies = this.searchMovies.bind(this);
+
+        console.log(OMDB_API_KEY)
     }
 
-    renderRow(row){
-        return(
+    searchMovies = debounce(text => {
+        fetch(`http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&plot=full&s=` + text)
+            .then((response) => response.json())
+            .then((responseData) => {
+                if('Search' in responseData){
+                    console.log(responseData);
+                    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+                    this.setState({
+                        dataSource: ds.cloneWithRows(responseData.Search)
+                    })
+                }
+
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, 500);
+
+    renderRow(row) {
+        return (
             <View style={styles.listItem}>
-                <Text>{row}</Text>
+                <Image source={{uri: row.Poster}} style={styles.poster}/>
+                <View style={{flex: 1}}>
+                    <Text style={styles.title}>{row.Title}</Text>
+                    <Text style={styles.subHeading}>{row.Type}-{row.Year}</Text>
+                </View>
+
             </View>
         )
     }
@@ -29,8 +60,8 @@ export default class Main extends Component<{}> {
         return (
             <View style={styles.container}>
                 <TextInput
-                    style={{height:40,borderColor:'gray',borderWidth:1}}
-                    onChangeText={(text) => this.setState({text})}
+                    style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+                    onChangeText={this.searchMovies}
                     placeholder="Enter search keyword"
                 />
                 <ListView
@@ -47,9 +78,21 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         backgroundColor: '#F5FCFF',
-        marginTop:25
+        marginTop: 25
     },
-    listItem:{
-        margin:10
+    listItem: {
+        margin: 10
+    },
+    poster: {
+        height: 75,
+        width: 50
+    },
+    title: {
+        margin: 5,
+        fontSize: 15
+    },
+    subHeading: {
+        margin: 5,
+        fontSize: 12
     }
 });
